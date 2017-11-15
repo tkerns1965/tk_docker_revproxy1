9e7501fe2498286e211d3a38a7fef0f6a16096d1
tkerns1965 <tony.kerns.65@gmail.com>
2017-11-15 14:56:06 -0500

    Update startup.sh

--- nginx_rp/startup.sh
+++ nginx_rp/startup.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
 crond -f
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start crond: $status"
+  exit $status
+fi
+
 nginx -g "daemon off;"
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start nginx: $status"
+  exit $status
+fi

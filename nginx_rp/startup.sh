4a205ce1946473066de8fd21f781b0578127ed20
tkerns1965 <tony.kerns.65@gmail.com>
2017-11-16 12:28:05 -0500

    Update startup.sh

--- nginx_rp/startup.sh
+++ nginx_rp/startup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-CMD ["s6-svscan", "/s6"]
+s6-svscan /s6
+echo "Hello startup.sh!"
+# certbot certonly --webroot -n --staging --dry-run --agree-tos -m tony.kerns.65@gmail.com -w /var/www/html -d tkhome.ignorelist.com,owncloud.tkhome.ignorelist.com && \
